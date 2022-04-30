88dcc4df7e5c83ac2317eb0842ea265c33125570
java <harsh20gupta00@gmail.com>
2022-04-30 14:57:09 +0530

    Second Commit

--- src/com/harsh/study.java
+++ src/com/harsh/study.java
@@ -1,9 +1,17 @@
 package com.harsh;
 
+import java.util.Scanner;
+
 public class study {
     public static void main(String[] args){
-
-        System.out.println("hello world");
+// To check number is even or odd
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        if(x % 2 == 0){
+            System.out.println("Number is even");
+        }else{
+            System.out.println("Number is odd");
+        }
     }
 
 }
